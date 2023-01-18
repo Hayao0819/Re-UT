c2fc1d2894aa48e33cff4fbd3c639de98d0c5513
hayao <hayao@fascode.net>
2023-01-18 21:07:24 +0900

    Add: Download jawiki-titles

---
+++ src/jawiki-titles/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+project_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)"
+
+# shellcheck source=../common.sh
+source "${project_dir}/src/common.sh"
+work_dir="$(make_workdir "${BASH_SOURCE[0]}")"
+
+get_dict(){
+    local downloaded_file
+    msg_info "Getting dictionary file from Wikipedia..."
+    downloaded_file=$(download_file "$work_dir" "https://dumps.wikimedia.org/jawiki/latest/jawiki-latest-all-titles-in-ns0.gz")
+    
+
+    # extract file
+    msg_info "Extracting downloaded file..."
+    7z x -aos "$downloaded_file" -o"$work_dir" >&2
+
+    echo "$work_dir/jawiki-latest-all-titles-in-ns0"
+}
+
+main(){
+    get_dict
+}
+
+main
